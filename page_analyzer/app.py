97695f4fa57980cd8d22d7348a8faa797fd71647
BogdanBarylo <barylob@gmail.com>
2023-11-18 08:29:25 +0300

    fix env, app

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 
 @app.route('/')
