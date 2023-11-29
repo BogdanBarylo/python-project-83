106a65603cdb5ff3c9b2d5c7d05669acd3919118
BogdanBarylo <barylob@gmail.com>
2023-11-29 10:46:42 +0300

    finished 4 step, fixed all_urls, fixed url, added 505

--- database.sql
+++ database.sql
@@ -3,3 +3,13 @@ id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 name varchar(255) NOT NULL UNIQUE,
 created_at timestamp DEFAULT NOW()
 );
+
+CREATE TABLE url_checks(
+id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+url_id bigint REFERENCES urls (id),
+status_code integer,
+h1 varchar(255),
+title varchar(255),
+description varchar(255),
+created_at timestamp DEFAULT NOW()
+);
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,4 @@
-from flask import (Flask, render_template, flash,
-                   get_flashed_messages, request, redirect, url_for)
+from flask import Flask, render_template, flash, request, redirect, url_for, session
 import os
 from dotenv import load_dotenv
 from page_analyzer.validator import validate_url
@@ -14,6 +13,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
+
+
+@app.errorhandler(505)
+def get_error(error):
+    return render_template('505.html'), 505
 
 
 @app.errorhandler(404)
@@ -52,20 +56,47 @@ def get_add_url():
 
 @app.get('/url/<int:id>')
 def get_url(id):
+    checks = session.get('checks')
+    session.clear()
     with conn.cursor(cursor_factory=DictCursor) as curs:
         curs.execute('SELECT name, created_at FROM urls WHERE id = %s', (id,))
         url = curs.fetchone()
     if not url['name'] or not url['created_at']:
         return redirect(url_for('page_not_found'))
     url['created_at'] = get_normalized_time(url['created_at'])
-    return render_template('url.html', id=id, **url)
+    return render_template('url.html', id=id, checks=checks, **url)
 
 
 @app.route('/urls')
 def get_all_urls():
     with conn.cursor(cursor_factory=DictCursor) as curs:
-        curs.execute('SELECT * FROM urls')
+        curs.execute('SELECT urls.id, urls.name, urls.created_at, MAX(url_checks.created_at) AS last_checked_at FROM urls LEFT JOIN url_checks ON urls.id = url_checks.url_id GROUP BY urls.id, urls.name, urls.created_at ORDER BY urls.id DESC')
         urls = curs.fetchall()
-    for url in urls:
-        url['created_at'] = get_normalized_time(url['created_at'])
+        for url in urls:
+            url['last_checked_at'] = get_normalized_time(url['last_checked_at'])
     return render_template('all_urls.html', urls=urls)
+
+
+
+@app.post('/urls/<int:id>/checks')
+def get_check_url(id):
+    with conn.cursor(cursor_factory=DictCursor) as curs:
+        curs.execute('SELECT name FROM urls WHERE id = %s', (id,))
+        url = curs.fetchone()
+    with conn.cursor(cursor_factory=DictCursor) as curs:
+        curs.execute('INSERT INTO url_checks (url_id) VALUES (%s)', (id,))
+        curs.connection.commit()
+
+    with conn.cursor(cursor_factory=DictCursor) as curs:
+        curs.execute('SELECT id, created_at FROM url_checks WHERE url_id = %s', (id,))
+        checks = curs.fetchall()
+    if not checks:
+        flash('Произошла ошибка при проверке', 'error')
+        return redirect(url_for('get_url', id=id))
+    
+    session['checks'] = [{'id': check['id'], 'created_at': get_normalized_time(check['created_at'])} for check in checks]
+    flash('Страница успешно проверена', 'success')
+    return redirect(url_for('get_url', id=id))
+
+
+
