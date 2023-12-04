b50c6371ac9a966fda2e482994bb981bd4aba265
BogdanBarylo <barylob@gmail.com>
2023-12-04 21:40:29 +0300

    try to fix ci

--- page_analyzer/normalize.py
+++ page_analyzer/normalize.py
@@ -2,9 +2,6 @@ from urllib.parse import urlparse
 
 
 def get_normalized_url(url):
-    if url:
-        normalized_url = urlparse(url)
-        result = "{}://{}".format(normalized_url.scheme, normalized_url.netloc)
-    else:
-        result = ''
+    normalized_url = urlparse(url)
+    result = f"{normalized_url.scheme}://{normalized_url.netloc}"
     return result
